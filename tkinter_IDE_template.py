856108692b707971f119f576a1e0af30fefe08ce
aboo-anas <66027861+aboo-anas@users.noreply.github.com>
2022-07-07 09:59:05 +0100

    Update tkinter_IDE_template.py

--- tkinter_IDE_template.py
+++ tkinter_IDE_template.py
@@ -5,7 +5,12 @@ root = tk.Tk()
 root.geometry('600x600')
 root.title('Tkinter Simple IDE')
 
+widget_frame = tk.Frame(root, bg='#272727')
 
+widget_frame.pack_propagate(False)
+widget_frame.configure(width=100)
+
+widget_frame.pack(side=tk.LEFT, fill=tk.Y)
 
 
 
