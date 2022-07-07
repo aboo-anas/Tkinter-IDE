035f197faf9212ccfa0c017f2e0422bb39065b5f
aboo-anas <66027861+aboo-anas@users.noreply.github.com>
2022-07-07 10:07:22 +0100

    Update tkinter_IDE_template.py

--- tkinter_IDE_template.py
+++ tkinter_IDE_template.py
@@ -37,53 +37,14 @@ clear_btn = tk.Button(widget_frame, text='Clear', bg='#272727', fg='red', font=(
                       command= btn_clear)
 clear_btn.pack(pady= 5, fill=tk.X)
 
-btn1 = tk.Button(widget_frame, text=Widgets[0][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                 command= lambda: insert_widget_code(Widgets[0][1]))
-btn1.pack(pady= 5, fill=tk.X)
+#Create Widget buttons
+wn = 0
+for i in Widgets:
 
-btn2 = tk.Button(widget_frame, text=Widgets[1][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                 command= lambda: insert_widget_code(Widgets[1][1]))
-btn2.pack(pady= 5, fill=tk.X)
-
-btn3 = tk.Button(widget_frame, text=Widgets[2][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                 command= lambda: insert_widget_code(Widgets[2][1]))
-btn3.pack(pady= 5, fill=tk.X)
-
-btn4 = tk.Button(widget_frame, text=Widgets[3][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                 command= lambda: insert_widget_code(Widgets[3][1]))
-btn4.pack(pady= 5, fill=tk.X)
-
-btn5 = tk.Button(widget_frame, text=Widgets[4][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                 command= lambda: insert_widget_code(Widgets[4][1]))
-btn5.pack(pady= 5, fill=tk.X)
-
-btn6 = tk.Button(widget_frame, text=Widgets[5][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                 command= lambda: insert_widget_code(Widgets[5][1]))
-btn6.pack(pady= 5, fill=tk.X)
-
-btn7 = tk.Button(widget_frame, text=Widgets[6][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                 command= lambda: insert_widget_code(Widgets[6][1]))
-btn7.pack(pady= 5, fill=tk.X)
-
-btn8 = tk.Button(widget_frame, text=Widgets[7][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                 command= lambda: insert_widget_code(Widgets[7][1]))
-btn8.pack(pady= 5, fill=tk.X)
-
-btn9 = tk.Button(widget_frame, text=Widgets[8][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                 command= lambda: insert_widget_code(Widgets[8][1]))
-btn9.pack(pady= 5, fill=tk.X)
-
-btn10 = tk.Button(widget_frame, text=Widgets[9][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                  command= lambda: insert_widget_code(Widgets[9][1]))
-btn10.pack(pady= 5, fill=tk.X)
-
-btn11 = tk.Button(widget_frame, text=Widgets[10][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                  command= lambda: insert_widget_code(Widgets[10][1]))
-btn11.pack(pady= 5, fill=tk.X)
-
-btn12 = tk.Button(widget_frame, text=Widgets[11][0], bg='#272727', fg='yellow', font=('Bold', 13),
-                  command= lambda: insert_widget_code(Widgets[11][1]))
-btn12.pack(pady= 5, fill= tk.X)
+    btn = tk.Button(widget_frame, text=Widgets[wn][0], bg='#272727', fg='yellow', font=('Bold', 13),
+                 command= lambda wn = wn: insert_widget_code(Widgets[wn][1]))
+    btn.pack(pady= 5, fill=tk.X)
+    wn+=1
 
 
 root.mainloop()
